a70e676caf7f476d8997c9c9cf71816a3f5174f3
WinSun <35953131+fiyen@users.noreply.github.com>
2023-08-27 15:05:56 +0800

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,6 +2,9 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
+TZ=Asia/Shanghai
+export TZ
+
 
 CURL_CHECK=$(which curl)
 if [ "$?" == "0" ];then
